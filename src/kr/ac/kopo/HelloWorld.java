c14da806fdca73fc1764fe2d56b9c42cdd8f2bcb
leeeojin <seoulm_s@naver.com>
2021-03-04 11:43:33 +0900

    g

---
+++ src/kr/ac/kopo/HelloWorld.java
@@ -0,0 +1,11 @@
+package kr.ac.kopo;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("good man!");
+		System.out.println("run!");
+	}
+
+}
